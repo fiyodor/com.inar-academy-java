c6e235e3f552e5bd38fa75e80f1ec2d6b446ca48
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-10-04 23:29:42 +0300

    Exercises 13, 14, 16, 17, 18, 19, 20, 21, 23, 26, 27, 28, 29, 35, 36, 37, 38 added to Chapter_06

---
+++ src/chapters/chapter_06/Exercise_20.java
@@ -0,0 +1,37 @@
+package chapters.chapter_06;
+
+import java.util.Scanner;
+
+public class Exercise_20 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a String: ");
+
+        String str = input.nextLine();
+
+        System.out.println("Number of letters in the String '" + str + "' is " + countLetters(str));
+
+
+
+    }
+
+    public static int countLetters(String s){
+
+        s = s.toUpperCase();
+
+        int letterCounter= 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+           if (s.charAt(i) >= 65 && s.charAt(i) <= 90){
+
+               letterCounter++;
+           }
+
+        }
+        return letterCounter;
+    }
+}
---
+++ src/chapters/chapter_06/Exercise_21.java
@@ -0,0 +1,60 @@
+package chapters.chapter_06;
+
+import java.util.Scanner;
+
+public class Exercise_21 {
+
+    public static void main(String[] args) {
+
+        Exercise_21 exercise_21 = new Exercise_21();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a phone number as a String: ");
+
+        String phoneNumber = input.nextLine();
+
+        phoneNumber = phoneNumber.toUpperCase();
+
+        String result = "";
+
+        for (int i = 0; i < phoneNumber.length(); i++) {
+
+            if (phoneNumber.charAt(i) >= 65 && phoneNumber.charAt(i) <= 90){
+
+                result += exercise_21.getNumber(phoneNumber.charAt(i));
+            }
+            else{
+
+                result += phoneNumber.charAt(i);
+            }
+
+        }
+        System.out.println(result);
+    }
+
+    int getNumber(char uppercaseLetter){
+
+        if (uppercaseLetter >= 'A' && uppercaseLetter <= 'C')
+            return 2 ;
+        else if (uppercaseLetter >= 'D' && uppercaseLetter <= 'F')
+            return 3 ;
+        else if (uppercaseLetter >= 'G' && uppercaseLetter <= 'I')
+            return 4 ;
+        else if (uppercaseLetter >= 'J' && uppercaseLetter <= 'L')
+            return 5 ;
+        else if (uppercaseLetter >= 'M' && uppercaseLetter <= 'O')
+            return 6 ;
+        else if (uppercaseLetter >= 'P' && uppercaseLetter <= 'S')
+            return 7 ;
+        else if (uppercaseLetter >= 'T' && uppercaseLetter <= 'V')
+            return 8 ;
+        else
+            return 9 ;
+
+
+
+    }
+
+
+}
