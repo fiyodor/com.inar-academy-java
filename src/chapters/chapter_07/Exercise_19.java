d26b576459862be69df1a17b29db325df47e6194
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-10-14 18:13:54 +0300

    Exercise_19 added to Chapter_07

---
+++ src/chapters/chapter_07/Exercise_19.java
@@ -0,0 +1,61 @@
+package chapters.chapter_07;
+
+import java.util.Scanner;
+
+public class Exercise_19 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the list: ");
+
+        final int array_Length = input.nextInt();
+
+        int[] array = new int[array_Length];
+
+        fillArrayWithInputs(array);
+
+        System.out.println("The list is " + (isSorted(array) ? "already sorted" : "not sorted"));
+
+
+    }
+    public static void fillArrayWithInputs(int[] array) {
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i++) {
+
+            array[i] = input.nextInt();
+        }
+    }
+
+    public static boolean isSorted(int[] array) {
+
+        return isSortedDecreasing(array) || isSortedIncreasing(array);
+    }
+    public static boolean isSortedDecreasing(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+            if (array[i] < array[i + 1]){
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSortedIncreasing(int[] array){
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+            if (array[i] > array[i + 1]){
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
