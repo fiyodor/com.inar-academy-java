765132973cad5ea4663ec8a8b264735d23061d9a
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-10-24 16:45:02 +0300

    Exercise_35 added to Chapter_08

---
+++ src/chapters/chapter_08/Exercise_35.java
@@ -0,0 +1,86 @@
+package chapters.chapter_08;
+
+import java.util.Scanner;
+
+public class Exercise_35 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows in the matrix: ");
+
+        final int ROW_AND_COL = input.nextInt();
+
+        int[][] array = new int[ROW_AND_COL][ROW_AND_COL];
+
+        System.out.println("Enter the matrix row by row");
+
+        fillArrayWithInputs(array);
+
+        int[] theLargestBlock = findLargestBlock(array);
+
+        System.out.printf("The maximum square matrix is at (%d, %d) with size %d", theLargestBlock[0], theLargestBlock[1], theLargestBlock[2]);
+
+    }
+    public static void fillArrayWithInputs(int[][] array){
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = 0; j < array[i].length ; j++) {
+
+                array[i][j] = input.nextInt();
+            }
+        }
+    }
+
+    public static int[] findLargestBlock(int[][] m){
+
+        int[] result = new int[3];
+
+        int rowCol = 2;
+
+        for (int i = 0; i < m.length; i++) {
+
+            for (int j = 0; j < m[i].length; j++) {
+
+                while (isSquare(m, i, j, rowCol)){
+
+                    result[0] = i;
+
+                    result[1] = j;
+
+                    result[2] = rowCol;
+
+                    rowCol++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean isSquare(int[][] array, int row, int col, int size){
+
+        if (col + size > array[row].length){
+
+            return false;
+        }
+        if (row + size > array.length){
+
+            return false;
+        }
+        for (int i = row; i < row + size; i++) {
+
+            for (int j = col; j < col + size; j++) {
+
+                if (array[i][j] != 1){
+
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
