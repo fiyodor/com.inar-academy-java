d7ea88ddb9a72cb70db7341bcdaa08673c5e7b8d
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-11-10 19:09:55 +0300

    Exercises 11 and 12 added to Chapter_10, Exercise_10 modified

---
+++ src/chapters/chapter_10/Exercise_13/MyRectangle2DTest.java
@@ -0,0 +1,15 @@
+package chapters.chapter_10.Exercise_13;
+
+public class MyRectangle2DTest {
+
+    public static void main(String[] args) {
+
+        MyRectangle2D r1 = new MyRectangle2D(2, 2, 5.5, 4.9);
+
+        System.out.println("Area of r1: " + r1.getArea());
+        System.out.println("Perimeter of r1: " + r1.getPerimeter());
+        System.out.println(r1.contains(3, 3));
+        System.out.println(r1.contains(new MyRectangle2D(4, 5, 10.5, 3.2)));
+        System.out.println(r1.overlaps(new MyRectangle2D(3, 5, 2.3 ,5.4)));
+    }
+}
