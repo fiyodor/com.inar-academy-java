5c0a31a72ddca7cdf73de9d99bef0c632cffca09
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-11-14 00:12:09 +0300

    A review package created and some Exercises added

---
+++ src/reviewOfChapters/chapter_01/Exercise_01.java
@@ -0,0 +1,11 @@
+package reviewOfChapters.chapter_01;
+
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
---
+++ src/reviewOfChapters/chapter_01/Exercise_02.java
@@ -0,0 +1,13 @@
+package reviewOfChapters.chapter_01;
+
+public class Exercise_02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
