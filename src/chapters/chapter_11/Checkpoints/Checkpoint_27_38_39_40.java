6e8295e08c0635f8c65f4899a52d14120d44812c
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-11-17 11:16:20 +0300

    Checkpoints added

---
+++ src/chapters/chapter_11/Checkpoints/Checkpoint_27_38_39_40.java
@@ -0,0 +1,18 @@
+package chapters.chapter_11.Checkpoints;
+
+public class Checkpoint_27_38_39_40 {
+
+    /*default package
+    *
+    *protected
+    *
+    *
+    * yes it can be compiled
+    * no it cant be compiled    private modifier can be accessed only from the same class
+    * yes it can be compiled
+    *
+    *
+    * no it cant be compiled
+    * no it cant be compiled
+    * yes it can be compiled. protected modifier allows Access from a subclass in a different package */
+}
---
+++ src/chapters/chapter_11/Checkpoints/Checkpoint_41_42.java
@@ -0,0 +1,18 @@
+package chapters.chapter_11.Checkpoints;
+
+public class Checkpoint_41_42 {
+
+    /*How do you prevent a class from being extended? How do you prevent a method from
+      being overridden?
+
+      // we can use final keyword in both cases
+
+
+        Indicate true or false for the following statements:
+        a. A protected datum or method can be accessed by any class in the same package.        true
+        b. A protected datum or method can be accessed by any class in different packages.      false (not any class. any class which extends the class whre we define protected)
+        c. A protected datum or method can be accessed by its subclasses in any package.        true
+        d. A final class can have instances.                                                    false
+        e. A final class can be extended.                                                       false
+        f. A final method can be overridden.                                                    false*/
+}
