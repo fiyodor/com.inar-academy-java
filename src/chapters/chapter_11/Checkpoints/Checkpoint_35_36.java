52085ba3b523e30d04ef98925f106009686ef99e
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-11-17 11:00:45 +0300

    Listing added

--- src/chapters/chapter_11/Checkpoints/Checkpoint_35_36.java
+++ src/chapters/chapter_11/Checkpoints/Checkpoint_35_36.java
@@ -15,6 +15,13 @@ public class Checkpoint_35_36 {
         list.add(1);
 
         //we must write list.add(1.0); because it is an arraylist of doubles
+
+
+        int[] array = {3, 5, 95, 4, 15, 34, 3, 6, 5};
+        System.out.println(java.util.Collections.max(array));
+
+        //array must be an object. if it was and object then we must use asList(array) method
+
     }
 }
 */
