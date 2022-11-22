2d90b932c33c0c69bd43ca9bd0c08f8d58b3e6f3
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-11-22 16:10:16 +0300

    Listings and Checkpoints added

---
+++ src/chapters/chapter_12/Checkpoints/Checkpoint_27_28_29.java
@@ -0,0 +1,22 @@
+package chapters.chapter_12.Checkpoints;
+
+public class Checkpoint_27_28_29 {
+
+    /*
+    * 27
+    *
+    * '\' is a special character. it must be '\\'
+    *
+    * 28
+    *
+    * with exists() method we can check whether a file already exists
+    * with delete() method we delete a file
+    * with renameTo() method we can rename
+    * with length() method we can find its size
+    * with mkdir() and mkdirs() methods we can create a directory
+    *
+    * 29
+    *
+    * we cant use File class for I/O.it is only for getting information and changing files. creating a file object doesnt create a file on a disk
+    * */
+}
