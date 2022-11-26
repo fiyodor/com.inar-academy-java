eb5a24077f44796c62604daff883f6214502368b
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-11-26 16:23:49 +0300

    Exercise_21 added to Chapter_12

---
+++ src/chapters/chapter_12/Exercises/Exercise_21/Exercise_21.java
@@ -0,0 +1,34 @@
+package chapters.chapter_12.Exercises.Exercise_21;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exercise_21 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("C:\\Users\\90543\\workspace\\com.inar-academy-java\\src\\chapters\\chapter_12\\Exercises\\Exercise_21\\SortedStrings.txt");
+
+        String strToBeCompared1;
+        String strToBeCompared2 = null;
+
+        try (Scanner input = new Scanner(file)){
+
+            while (input.hasNext()){
+
+                strToBeCompared1 = input.next();
+
+                if(strToBeCompared2 != null){
+
+                    if(strToBeCompared2.compareToIgnoreCase(strToBeCompared1) > 0){
+                        System.out.println("The file is not sorted");
+                        System.exit(0);
+                    }
+                }
+                strToBeCompared2 = strToBeCompared1;
+            }
+            System.out.println("The file is sorted");
+        }
+    }
+}
