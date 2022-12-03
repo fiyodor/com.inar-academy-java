87a15494fc2f59ad18dd3dc0655a70981ff8db04
Ertugrul DONMEZ <ertugrul.donmez32@gmail.com>
2022-12-03 18:03:51 +0300

    Exercise 1 and 2 added to Chapter_13

---
+++ src/chapters/chapter_13/Checkpoints/Checkpoint_30_31_32_33_34.java
@@ -0,0 +1,30 @@
+package chapters.chapter_13.Checkpoints;
+
+import chapters.chapter_13.Listings.Listing_13_13_Rational;
+
+public class Checkpoint_30_31_32_33_34 {
+/*
+        Rational r1 = new Rational(-2, 6);
+        System.out.println(r1.getNumerator());          -1
+        System.out.println(r1.getDenominator());         3
+        System.out.println(r1.intValue());               0
+        System.out.println(r1.doubleValue());            0.333333333333333333
+
+
+
+        because of r2 being Object type, it doesnt have the compareTo()
+
+
+        because of r1 being Object type, it doesnt have the compareTo()
+
+        return ((this.subtract((Rational)(other))).getNumerator() == 0);
+
+ */
+   /* public static void main(String[] args) {
+
+    Listing_13_13_Rational r1 = new Listing_13_13_Rational(1, 2);
+    Listing_13_13_Rational r2 = new Listing_13_13_Rational(1, -2);
+    System.out.println(r1.add(r2));                                         0/4
+}
+*/
+}
